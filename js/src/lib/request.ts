f654bd1670b9055d731bd70bf5d5ffe507ebbfc1
swadical <swadical@lab.glua.team>
2020-02-13 20:35:36 +1030

    rebuild w/ updated whateverorigin endpoint

--- js/src/lib/request.ts
+++ js/src/lib/request.ts
@@ -16,7 +16,7 @@ export function get(url,callback:(contents:string,code:number) => void) {
 
     window[cb] = callback2;
 
-    $("body").append("<script src='https://whateverorigin.herokuapp.com/get?url=" + encodeURIComponent(url) + "&callback=" + cb + "'></script>");
+    $("body").append("<script src='http://www.whateverorigin.org/get?url=" + encodeURIComponent(url) + "&callback=" + cb + "'></script>");
 }
 
 export interface IResponse {
@@ -40,5 +40,5 @@ export function getPromise(url) {
 
 //     window[cb] = callback;
 
-//     $("body").append("<script src='https://whateverorigin.herokuapp.com/post?url=" + encodeURIComponent(url) + "&callback=" + cb + "'></script>");
+//     $("body").append("<script src='http://www.whateverorigin.org/post?url=" + encodeURIComponent(url) + "&callback=" + cb + "'></script>");
 // }
